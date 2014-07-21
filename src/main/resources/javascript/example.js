44b31f867ca751ba64a975f706f2a607a12856a7
purplefox <timvolpe@gmail.com>
2014-07-21 09:41:35 +0100

    Some comments

--- src/main/resources/javascript/example.js
+++ src/main/resources/javascript/example.js
@@ -3,9 +3,17 @@ var MongoDBService = require('mongodb-js/mongo_dbservice.js');
 
 console.log("Starting");
 
+// First lets deploy our MongoDB module
 vertx.deployVerticle("java:io.vertx.mongodb.MongoDBServiceVerticle", {}, function(deploymentID, err) {
   console.log("deployment id is " + deploymentID);
+
+
+  // Now lets create a local proxy to it, which works by sending messages to and from the actual verticle over the
+  // event bus, but you interact with it using a rich idiomatic API
   var mongoDBService = MongoDBService.create(vertx, {});
+
+
+  // Execute an operation on the service
   mongoDBService.find("foo", {}, function(res, err2) {
     console.log("result is: " + JSON.stringify(res));
   });
